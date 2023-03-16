d7d0bca8aea5e455a79f8e35c26a85bfbc2f4b80
Bright <brightdjogatse@gmail.com>
2023-03-16 15:57:31 -0700

    Bright

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 
 /**
  
- * main -Entry point
- * *Return = 0
- * */
-int main(void)
+ * main - Entry point
+ * *Return  0
+ * /
+
+ int main(void)
 {	
 	puts("/"Programming is like building a multilingual puzzle");
 	
 	return (0);
-
 }	
