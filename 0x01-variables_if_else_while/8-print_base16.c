8c435f7d123a7514845f28b7c602217ed03379f1
Bright <brightdjogatse@gmail.com>
2023-03-18 07:10:28 -0700

    Bright

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	for (n = 97; m <= 102; m++)
+	for (n = 97; m < 103; m++)
 	{
 		putchar(m);
 	}
