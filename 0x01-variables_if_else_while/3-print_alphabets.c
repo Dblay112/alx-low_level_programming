0041454d2e583ecbdd5786861553cda66937c0a4
Bright <brightdjogatse@gmail.com>
2023-03-17 19:20:23 -0700

    Bright

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,16 @@
 int main(void)
 {
 	int n = 97;
+	int m= 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
-		n++;
 	}
-	putchar('\n');
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	putchar ('\n);
 	return (0);
 }
