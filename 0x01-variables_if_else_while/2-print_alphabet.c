27251c6a2197efb40b34d9cc418852436b818279
Bright <brightdjogatse@gmail.com>
2023-03-17 19:39:37 -0700

    Bright

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * *description: 'prints the alphabet in lowercase
- * and then in uppercase, followed by a new line.
+ * Des: 'prints the alphabet in lowercase'
  * Return: Always 0
  */
 int main(void)
 {
-        int n = 97;
-	int m = 65;
+	int n = 97;
 
-        while (n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
-	while (m <= 90)
+	while (n <= 122)
 	{
-		putchar(m);
-		m++;
+		putchar(n);
+		n++;
 	}
-
-        putchar('\n');
-        return (0);
+	return (0);
 }
