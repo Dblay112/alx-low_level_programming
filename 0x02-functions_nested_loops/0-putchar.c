dce342c62a9610ec7a15b1037b3d6808f79193f7
Bright <brightdjogatse@gmail.com>
2023-03-21 08:20:01 -0700

    Bright

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char br  = "_putchar";
+
+	while (br)
+	{
+		_putchar(br);
+		br++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
