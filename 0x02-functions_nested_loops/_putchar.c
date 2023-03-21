8e8c021923754af3599cf3d065f5668bef0d1f21
Bright <brightdjogatse@gmail.com>
2023-03-21 09:06:28 -0700

    Bright

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ *_putchar - writes the char c to stndrd output
+ *
+ * Return: Always 1 (Success)
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));:
+
+
+}
