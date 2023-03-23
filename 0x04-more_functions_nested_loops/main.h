a3ce357ba664519b287860372b7b07c6a1a21f64
Bright <brightdjogatse@gmail.com>
2023-03-23 09:02:21 -0700

    Bright

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int multiply(int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
