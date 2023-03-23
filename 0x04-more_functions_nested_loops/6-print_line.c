178450c3ffa877ca5a247e7a2789cbda566ca1af
Bright <brightdjogatse@gmail.com>
2023-03-23 08:04:40 -0700

    Bright

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int b)
+{
+	int len;
+
+	if (b > 0)
+	{
+		for (len = 0; len < b; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
