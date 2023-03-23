dd9f68427ec773ed0abedc7cb9427f027dcee026
Bright <brightdjogatse@gmail.com>
2023-03-23 08:12:54 -0700

    Bright

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#include <stdion.h>
-
 
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
+int multiply(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
