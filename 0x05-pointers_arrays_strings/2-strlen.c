80ffe1f8e4f475d4cccd8ae75f02748e377811ef
Bright <brightdjogatse@gmail.com>
2023-03-27 10:47:42 -0700

    Bright

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string.
+ *
+ * Return: length of the the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _strcpy - Copies a string pointed to by src, including the
+ *           terminating null byte, to a buffer pointed to by dest.
+ * @dest: buffer storing the string copy.
+ * @src: source string.
+ *
+ * Return: pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0, len = 0;
+
+	while (src[index++])
+		len++;
+
+	for (index = 0; index < len; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
