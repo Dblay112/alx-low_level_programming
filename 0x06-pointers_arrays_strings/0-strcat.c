fd3d7f851f3bbdf348adc123fa63d0881ffb0074
Bright <brightdjogatse@gmail.com>
2023-03-30 00:42:29 -0700

    Bright

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,26 @@
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
- * Return: A pointer to the destination string @dest.
+ * Return: A pointer to the destination string @dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src);
 {
-	int index = 0, dest_len = 0;
+	int b;
+	int d;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	int b = 0;
+	while (dest[b] != '\0')
+	{
+		b++;
+	}
+	d = 0;
+	while (src[d] != '\0')
+	{
+		dest[b] = src[d];
+		b++;
+		d++;
+	}
+	dest[b] = '\0';
 	return (dest);
 }
+
