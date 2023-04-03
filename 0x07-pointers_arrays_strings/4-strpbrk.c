1e9558b5dc982a909ab9e7b444ade724a130443c
Bright <brightdjogatse@gmail.com>
2023-04-03 10:13:18 -0700

    Bright

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  locates the first occurrence in the string
+ * @s: string of any byte
+ * @accept: input
+ * Return: Always 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int i;
+
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+			if (*s == accept[i])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
