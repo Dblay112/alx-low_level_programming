760c5604f0abc22bb4ae165aa4117fdea7e09da1
Bright <brightdjogatse@gmail.com>
2023-04-08 15:39:50 -0700

    Bright

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,20 +2,21 @@
 #include "main.h"
 
 /**
- * main - prints the number of arguments passed
- * @argc: number of arguments
+ * main - prints arguments received
+ * @argc: no of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-#include <stdio.h>
-
 int main(int argc, char *argv[])
 {
 	int b;
-	for(b=0; b<argc; b++)
-		printf("argv[%2d]: %s\n",b,argv[b]);
-	
-	return 0;
+
+	for (b = 0; b < argc; b++)
+	{
+		printf("%s\n", argv[b]);
+	}
+
+	return (0);
 }
