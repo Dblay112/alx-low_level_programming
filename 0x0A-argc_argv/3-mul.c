7d6c41889ce3cd77fb534c4e5c9883b01e9c0405
Bright <brightdjogatse@gmail.com>
2023-04-08 15:57:30 -0700

    Bright

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,51 @@
 #include "main.h"
 
 /**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, bri, d, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	bri = 0;
+	d = 0;
+	digit = 0;
+
+	while (s[bri] != '\0')
+		bri++;
+
+	while (i < bri && f == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			b = 0;
+		}
+		i++;
+	}
+
+	if (b == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -10,7 +55,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result, digi1, digi2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -18,11 +63,14 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	digi1 = _atoi(argv[1]);
+	digi2 = _atoi(argv[2]);
+	result = digi1 * digi2;
 
 	printf("%d\n", result);
 
 	return (0);
 }
+
+
+
