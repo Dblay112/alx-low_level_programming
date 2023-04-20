1d2e52996f964d60e04e607886d5a61c9fea7ca8
Bright <brightdjogatse@gmail.com>
2023-04-20 14:32:00 -0700

    C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,11 +21,11 @@ void print_all(const char * const format, ...)
 			{
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
-					break
+					break;
 				case 'i':
 						printf("%s%d", sep, va_arg(list, int));
 						break;
-				case 'f';
+				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
