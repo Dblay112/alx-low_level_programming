d0de633c024cf32b79cae36973c05ddd05e16f5c
Bright <brightdjogatse@gmail.com>
2023-05-01 06:49:06 -0700

    advanced tasks

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to first node in the list
+ *
+ * Return: pointer to first node in new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *bullet, *bomb;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	bomb = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		bullet = (*head)->next;
+		(*head)->next = bomb;
+		bomb = *bullet;
+		*head = bullet;
+	}
+
+	(*head)->next = bomb;
+
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds loop contained in listint_t linked list.
+ * @head: pointer to the head of the listint_t list.
+ *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - address of node where loop starts.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *bullet, *bomb;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	bullet = head->next;
+	bomb = (head->next)->next;
+
+	while (bomb)
+	{
+		if (bullet == bomb)
+		{
+			bullet = head;
+
+			while (bullet != hare)
+			{
+				bullet = bullet->next;
+				bomb = bomb->next;
+			}
+
+			return (bullet);
+		}
+
+		bullet = bullet->next;
+		bomb = (bomb->next)->next;
+	}
+
+	return (NULL);
+}
