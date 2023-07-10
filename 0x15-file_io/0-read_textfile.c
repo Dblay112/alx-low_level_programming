81c7fab89959f29f919e5a52e241045049e73258
dblay112 <brightdjogatse@gmail.com>
2023-07-10 08:38:26 +0000

    file I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+#include <stdlib.h>
+
+/**
+ *  * read_textfile- prints to STDOUT from Read text file 
+ *   * @filename: this is the file text being read
+ *    * @letters: letter numbers to read
+ *     * Return: w- return w when successful and -1 when error
+ *      */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+nclude "main.h"
+
+/**
+ *  * append_text_to_file - end of file appendixation
+ *   * @filename: this is the name of the file pointer
+ *    * @text_content: end of file string to be added
+ *     *
+ *      * Return: return 1 on success and -1 and error
+ *       */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
