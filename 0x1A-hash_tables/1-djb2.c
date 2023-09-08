20fd49e65147aa56c8a1e48e695332f3db320ae9
dblay112 <brightdjogatse@gmail.com>
2023-09-08 02:46:31 -0700

    djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,7 +8,15 @@
  *
  * Return: key index.
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
