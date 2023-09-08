86f5f46dfb83ca9f132dd5c138a1da9bcb326532
dblay112 <brightdjogatse@gmail.com>
2023-09-08 03:04:14 -0700

    key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,53 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - func to add/update element in a hash table.
- * @ht: hash table pointer
- * @key: key to be added- cannot be empty string.
- * @value: value of the key
+ * key_index - function that to get the index of a key
  *
- * Return: on success 1.
- *         failure - 0
+ * Description: gets the index of a key
+ *
+ * @key: the key if the func
+ * @size: size of the array of the hash table
+ *
+ * Return: key index
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_node_t *new;
-	char *v_copy;
-	unsigned long int index, i;
+	unsigned long int hash_rs;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	v_copy = strdup(value);
-	if (v_copy == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = v_copy;
-			return (1);
-		}
-	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(v_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = v_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	return (1);
+	hash_rs = hash_djb2(key);
+	if (size != 0)
+		return (hash_rs % size);
+	return (0);
 }
